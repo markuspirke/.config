beb8f0791eacce0dfcaa6ae4d1aff4df7b1570c0
Markus Pirke <markus.pirke@fau.de>
2025-09-29 10:50:02 +0200

    update sketchybar

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export WHITE=0xffffffff
+export GREEN=0xff1dfca1
 
 # -- Teal Scheme --
 #export BAR_COLOR=0xff001f30
@@ -13,14 +14,14 @@ export WHITE=0xffffffff
 # export ACCENT_COLOR=0xffffffff
 
 # -- Purple Scheme --
-#export BAR_COLOR=0xff140c42
-#export ITEM_BG_COLOR=0xff2b1c84
-#export ACCENT_COLOR=0xffeb46f9
+export BAR_COLOR=0xff140c42
+export ITEM_BG_COLOR=0xff2b1c84
+export ACCENT_COLOR=0xffeb46f9
 
 # -- Red Scheme ---
-export BAR_COLOR=0xff23090e
-export ITEM_BG_COLOR=0xff591221
-export ACCENT_COLOR=0xffff2453
+#export BAR_COLOR=0xff23090e
+#export ITEM_BG_COLOR=0xff591221
+#export ACCENT_COLOR=0xffff2453
 
 # -- Blue Scheme ---
 # export BAR_COLOR=0xff021254
--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -6,17 +6,3 @@ else
     sketchybar --set $NAME background.drawing=off
 fi
 
-sketchybar --add event aerospace_workspace_change
-
-for sid in $(aerospace list-workspaces --all); do
-    sketchybar --add item space.$sid left \
-        --subscribe space.$sid aerospace_workspace_change \
-        --set space.$sid \
-        background.color=0x44ffffff \
-        background.corner_radius=5 \
-        background.height=20 \
-        background.drawing=off \
-        label="$sid" \
-        click_script="aerospace workspace $sid" \
-        script="$CONFIG_DIR/plugins/aerospace.sh $sid"
-done
